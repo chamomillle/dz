bfe85f149f967122a851e6fb93eca9bc0aa12977
chamomillle <marina10032005@yandex.ru>
2023-09-09 12:35:46 +0300

    dz1

---
+++ 1/1.py
@@ -0,0 +1,11 @@
+print ('введите 2 числа a и b')
+a=int(input())
+b=int(input())
+if a < b:
+    print ('2 число больше 1')
+if  a > b:
+    print ('1 число больше 2')
+if a == b:
+    print ('числа равны')
+
+
